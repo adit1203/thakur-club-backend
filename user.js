d2b2e4ffce5165101f579ef269c9843108a9ba7b
adit1203 <adityasinghltcl@gmail.com>
2025-06-29 13:54:20 +0530

    Delete user.js

--- user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  balance: Number,
-});
-
-module.exports = mongoose.model('User', userSchema);
